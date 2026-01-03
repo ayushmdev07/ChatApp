4a1896c3e82dba56ae4a66b0878c93ebfa7eb61e
Ayush Maurya <virendramaurya3237@gmail.com>
2026-01-03 09:52:59 +0530

    Add project files.

---
+++ SignalR/Hubs/MessageHub.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalR.Hubs
+{
+    public class MessageHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+                await Clients.All.SendAsync("ReceiveMessage",user, message);
+        }
+        public async Task Join(string user)
+        {
+            await Clients.All.SendAsync("UserJoined", user);
+        }
+        public async Task Leave(string user)
+        {
+            await Clients.All.SendAsync("UserLeft", user);
+        }
+    }
+}
---
+++ SignalR/Models/UserDetails.cs
@@ -0,0 +1,8 @@
+﻿namespace SignalR.Models
+{
+    public class UserDetails
+    {
+        public string User { get; set; }
+        public string Message { get; set; }
+    }
+}
